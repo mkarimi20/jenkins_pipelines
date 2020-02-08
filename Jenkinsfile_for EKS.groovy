58899ec0fdc46944b975245b667373db4c94d3cf
Mohammad Karimi <mkarimidevops@gmail.com>
2020-02-08 12:49:28 -0600

    eks file has been added

---
+++ Jenkinsfile_for EKS.groovy
@@ -0,0 +1,11 @@
+node {
+    stage("pull repo"){
+        git 'https://github.com/bkarakas/terraform-iaac-eks-burak.git'
+    }
+     stage("Build Image"){
+    }
+     stage("Build Image"){
+    }
+     stage("Build Image"){
+    }
+}
