4b91b1ad22216d4c63c4f78755020d5252b53133
Mohammad Karimi <mkarimidevops@gmail.com>
2020-02-08 10:43:04 -0600

    choice parameter added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,5 @@
 node {
+    properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please select region', name: 'REGION_AMI')])])
     stage("pull repo"){
         echo "Hello"
 
