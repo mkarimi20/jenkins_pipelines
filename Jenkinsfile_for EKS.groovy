1a797ec88e4f211bf9428e01b828a4e093a6f066
Mohammad Karimi <mkarimidevops@gmail.com>
2020-02-08 13:43:53 -0600

    short cut added

--- Jenkinsfile_for EKS.groovy
+++ Jenkinsfile_for EKS.groovy
@@ -1,20 +1,25 @@
-node {
-    stage("pull repo"){
-        git 'https://github.com/mkarimi20/Jenkins-terraform-EKS.git'
+node{
+    stage("Pull Repo"){
+        ws ("tmp/"){
+            git 'https://github.com/mkarimi20/Jenkins-terraform-EKS.git'
+        }
+        
     }
-       stage("Download Terraform"){
-        sh "terraform version"
-        sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
-        sh "unzip terraform_0.12.19_linux_amd64.zip"
-        sh "./terraform version"
-        sh "chmod +x terraform"
-        sh "mv terraform* terraform-12"
-        sh "mv terraform-12 /sbin/"
+    stage("Download Terraform"){
+        ws ("tmp/") {
+            sh "terraform version"
+            sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
+            sh "unzip -o terraform_0.12.19_linux_amd64.zip"
+            sh "./terraform version"}
     }
-
-     stage("Build Image"){
-         sh "source setenv.sh configurations/dev/us-east-1/dev.tfvars"
+    stage("Set Backend"){
+        ws ("tmp/"){
+            sh "./terraform init"
+        }
     }
-     stage("Build Image"){
+    stage("Plan"){
+        ws ("tmp/") {
+            sh "./terraform plan -var-file configurations/dev/us-east-1/dev.tfvars"
+        }
     }
 }
