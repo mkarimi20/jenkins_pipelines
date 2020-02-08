0ab30b6b7d1a888e6b3760c4d923783a31bc10cf
Mohammad Karimi <mkarimidevops@gmail.com>
2020-02-08 13:34:58 -0600

    terrafrom binary changed

--- Jenkinsfile_for EKS.groovy
+++ Jenkinsfile_for EKS.groovy
@@ -7,8 +7,8 @@ node {
         sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
         sh "unzip terraform_0.12.19_linux_amd64.zip"
         sh "./terraform version"
-        sh "chmod +x terraform.*"
-        sh "mv terraform.* terraform-12"
+        sh "chmod +x terraform"
+        sh "mv terraform terraform-12"
         sh "mv terraform-12 /sbin/"
     }
 
