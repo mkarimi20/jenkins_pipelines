80cb9e58806fff881458ee339dda56823fa853f6
Mohammad Karimi <mkarimidevops@gmail.com>
2020-02-08 10:32:34 -0600

    files for packer

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,1 +1,14 @@
+node {
+    stage("pull repo"){
 
+    }
+     stage("build image"){
+         sh "packer version"
+    }
+     stage("send notification to slack"){
+        echo "Hello"
+    }
+     stage("send email"){
+        echo "Hello"
+    }
+}
