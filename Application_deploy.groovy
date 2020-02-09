066af6634cb16b6048d8bf1d9ad48203d25a8932
Mohammad Karimi <mkarimidevops@gmail.com>
2020-02-09 00:11:49 -0600

    add email para

--- Application_deploy.groovy
+++ Application_deploy.groovy
@@ -1,13 +1,18 @@
 node {
 	properties(
 		[parameters(
+        text(defaultValue: 'dummy@gmail.com', description: 'Please provide email(s) for notifications. Use , for multiple emails', name: 'EMAIL_TO_SEND'),xs
 		[choice(choices: 
 		[
 		'version/0.1', 
 		'version/0.2', 
 		'version/0.3', 
 		'version/0.4', 
-		'version/0.5'], 
+		'version/0.5',
+        'version/0.6',
+        'version/0.7',
+        'version/0.8',
+        'version/0.9'], 
 	description: 'Which version of the app should I deploy? ', 
 	name: 'Version'), 
 	choice(choices: 
@@ -18,7 +23,6 @@ node {
 		'prod1.acirrustech.com'], 
 	description: 'Please provide an environment to build the application', 
 	name: 'ENVIR')])])
-
 	stage("Stage1"){
 		timestamps {
 			ws {
@@ -30,9 +34,9 @@ node {
 		timestamps {
 			ws{
 				sh '''
-					ssh myjenkins@${ENVIR} sudo yum install epel-release -y
-					ssh myjenkins@${ENVIR} sudo yum install python-pip -y 
-					ssh myjenkins@${ENVIR} sudo pip install Flask
+					ssh centos@${ENVIR} sudo yum install epel-release -y
+					ssh centos@${ENVIR} sudo yum install python-pip -y 
+					ssh centos@${ENVIR} sudo pip install Flask
 					'''
 		}
 	}
@@ -41,7 +45,7 @@ node {
 		timestamps {
 			ws {
 				sh '''
-					scp -r * myjenkins@${ENVIR}:/tmp
+					scp -r * centos@${ENVIR}:/tmp
 					'''
 		}
 	}
@@ -50,7 +54,7 @@ node {
 		timestamps {
 			ws {
 				sh '''
-					ssh myjenkins@${ENVIR} nohup python /tmp/artemis.py  &
+					ssh centos@${ENVIR} nohup python /tmp/artemis.py  &
 					'''
 		}
 	}
@@ -63,4 +67,13 @@ node {
 			}
 		}
 	}
+     stage("Send Email"){
+        mail bcc: '', 
+        body: "Hello, Your AMI is ready in ${AMI_REGION} Thanks", 
+        cc: '', 
+        from: '', 
+        replyTo: '', 
+        subject: "${TOOL_TO_PROVISION} has been built", 
+        to: "${EMAIL_TO_SEND}"
+    }
 }
